af5b7d1b37fb21272ea384fe4a651519d361eefe
hojat sajadinia <hojat@hojat.com>
2024-05-11 03:44:30 +0330

    refacor: Scanner funcations refactored

--- utils/Scanner.py
+++ utils/Scanner.py
@@ -2,6 +2,7 @@ import os
 import re
 from utils.Const import Const
 from utils.ColorPrint import ColorPrint as cp
+
 class Scanner:
     def __init__(self) -> None:
         self.root_pattern_count = 0
@@ -9,15 +10,19 @@ class Scanner:
     
     def patterns_scanner(self,directory_path):
         self.patterns_scanner_implementation(directory_path)
-        if self.root_pattern_count != 0:
-            cp.pr("info", f"[INFO] {self.root_pattern_count} Pattern/s Detected.")
+        
+        if self.root_pattern_count:
+            cp.pr("info", f"[INFO] {self.root_pattern_count} Root Detection Pattern/s Detected.")
+        if self.emulator_pattern_count:
+            cp.pr("info", f"[INFO] {self.emulator_pattern_count} Emulator Detection Pattern/s Detected.")
+
+        
+        if self.root_pattern_count or self.emulator_pattern_count:
             return True
         else:
             cp.pr("error", "[ERROR] Unable to scann the application")
             return False
-        
-        
-
+      
     def patterns_scanner_implementation(self,directory_path):
         try:
             for root, _, files in os.walk(directory_path):
@@ -28,26 +33,25 @@ class Scanner:
                             file_content = file.read()
 
                             # Root Detection
-                            self.root_detection_bypass(file_content)
+                            file_content = self.root_detection_bypass(file_content)
 
                             # Emulator Detection
-                            self.emulator_detection_bypass(file_content)
+                            file_content = self.emulator_detection_bypass(file_content)
 
                         with open(file_path, 'w') as file:
                             file.write(file_content)
         except Exception as e:
-            #cp.pr("error", f"[ERROR] Unable to scan the application. {e}")
             pass
     
     def root_detection_bypass(self, file_content):
         for search_str in Const.all_root_values:
             self.root_pattern_count += file_content.count(search_str)
-            file_content = re.sub(search_str, f"{search_str[0]}X{search_str[2:]}", file_content)
+            return re.sub(search_str, f"{search_str[0]}X{search_str[2:]}", file_content)
     
     def emulator_detection_bypass(self, file_content):
         for search_str in Const.all_emulator_values:
             self.emulator_pattern_count += file_content.count(search_str)
-            file_content = re.sub(search_str, f"{search_str[0]}X{search_str[2:]}", file_content)
+            return re.sub(search_str, f"{search_str[0]}X{search_str[2:]}", file_content)
 
 
 
